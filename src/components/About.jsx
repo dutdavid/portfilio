8091005534530543c04321dc6cc85a18d0a857a3
Dut <dutdavid7@gmail.com>
2023-03-05 11:56:15 +0000

    descriptive text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,12 +9,20 @@ import { fadein, textVariant } from '../utils/motion';
 const About = () => {
   return (
     <>
-    <motion.div>
+    <motion.div variants={textVariant()}>
       <p className={sectionSubText}>
         Introduction
       </p>
       <h2 className={styles.sectionHeadText}>Overview.</h2>
     </motion.div>
+    <motion.p
+      variants={fadein("", "", 0.1, 1)}
+      >
+        A competent full stack and Mobile app developer with a proven track record in developing mobile applications, 
+        networking and managing databases. Very keen on acquiring knowledge to develop and improve myself. 
+        My career goal is to gain a role which allows me to further my knowledge and skills, 
+        as well as increased responsibility at a market-leading and innovative company.
+      </motion.p>
     </>
   )
 }
