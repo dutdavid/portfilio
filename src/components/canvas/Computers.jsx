60e643ff55eafdd25c1d5e522394cd0714f16e7d
Dut <dutdavid7@gmail.com>
2023-03-05 10:59:18 +0000

    media query

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,21 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500');
+    const mediaQuery = window.matchMedia(
+      '(max-width: 500');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change',
+    handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMediaQueryChange);
+    }
   }, [third])
 
   return (
