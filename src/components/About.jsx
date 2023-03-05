8c190d6d7750584aea9cf9e880acd615fa429b1d
Dut <dutdavid7@gmail.com>
2023-03-05 12:55:37 +0000

    Service Card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,6 +24,8 @@ const ServiceCard = ({ index, title, icon }) => {
       }}
       className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
+        <img src={icon} alt={title}
+        className="w-16 h-16 object-contain" />
 
       </div>
 
