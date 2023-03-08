9468778b42d2b2ffeea024c32d47bbfb3ed9ad37
Dut <dutdavid7@gmail.com>
2023-03-08 17:56:01 +0000

    fixing the cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,6 @@ const ServiceCard = ({ index, title, icon }) => {
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-
-      </motion.div>
       <div
       options={{
         max: 45,
@@ -28,6 +26,8 @@ const ServiceCard = ({ index, title, icon }) => {
         className="w-16 h-16 object-contain" />
 
       </div>
+      </motion.div>
+
 
     </Tilt>
   )
