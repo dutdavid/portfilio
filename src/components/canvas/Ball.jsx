12cd28e04eed8be2da3c43a2d85cbf630754caec
Dut <dutdavid7@gmail.com>
2023-03-09 17:59:57 +0000

    React Ball Canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/drei'
+import {
+  Decal, Float, OrbitControls, Preload, useTexture
+} from '@react-three/drei'
 
 const Ball = () => {
   return (
