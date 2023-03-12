a67b8004c6d8e27c4a6a33a85dc4fa0ae37c3f56
Dut <dutdavid7@gmail.com>
2023-03-12 17:20:48 +0000

    works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,18 @@ import { fadeIn, textVariant } from '../utils/motion';
 const Works = () => {
   return (
     <>
-    
+    <motion.div variants={textVariant()}>
+    <p className={styles.sectionSubText}>What I have done so far</p>
+         <h2 className={styles.sectionHeadText}>Work Experience</h2>
+    </motion.div>
+    <div className="mt-20 flex flex-col">
+     <VerticalTimeline>
+       {experiences.map((experience, index) => (
+         <ExperienceCard key={index} experience={experience} />
+       ))}
+     </VerticalTimeline>
+ 
+    </div>
     </>
   )
 }
